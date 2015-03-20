54275b44b05b366e19855fdec7d9a019de279bf4
OriginalEXE <antesepic@gmail.com>
2015-03-20 02:06:13 +0100

    Progress on main admin page layout and styling

--- admin/partials/amc-admin-plugin-page-display.php
+++ admin/partials/amc-admin-plugin-page-display.php
@@ -12,14 +12,30 @@
 
 ?>
 
-<div class="wrap amc-wrap">
+<div class="wrap wrap-amc">
 
 	<h2><?php _e( 'Admin Menu Control', 'admin-menu-control' ); ?></h2>
+
 	<h2 class="nav-tab-wrapper">
-		<a href="about.php" class="nav-tab nav-tab-active">
-			What’s New	</a><a href="credits.php" class="nav-tab">
-			Credits	</a><a href="freedoms.php" class="nav-tab">
-			Freedoms	</a>
+		<a href="options-general.php?page=admin-menu-control" class="nav-tab nav-tab-active">
+			<?php _e( 'Menu Control', 'admin-menu-control' ); ?>
+		</a>
+		<a href="options-general.php?page=admin-menu-control" class="nav-tab">
+			<?php _e( 'Settings', 'admin-menu-control' ); ?>
+		</a>
 	</h2>
+
+	<section class="tab-amc" data-content="menu-control">
+
+		<div class="menu-control-container-amc">
+			<header class="menu-control-container-amc__header">
+				<div class="h-pull-right-amc">
+					<?php submit_button( __( 'Save Menu', 'admin-menu-control' ) ); ?>
+				</div>
+			</header>
+			<footer class="menu-control-container-amc__footer"></footer>
+		</div>
+
+	</section>
 
 </div>
